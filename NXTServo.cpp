25493e8b277b4ad1e8c01fc22b4dfc862020c88c
sebtastik <sebktm@icloud.com>
2015-07-21 13:27:12 +0200

    big update!

--- NXTServo.cpp
+++ NXTServo.cpp
@@ -9,136 +9,98 @@
 #include "NXTServo.h"
 
 NXTServo::NXTServo(int fwdPin, int revPin, int interruptPin, int quadraturePin):
-Motor(fwdPin, revPin),
-Encoder(interruptPin, quadraturePin){
-    
-    
-    tuneAnglePID(0,0,0);
-    tuneSpeedPID(0,0,0);
-    
-    //default state
-    controllerMode_=OFF;
-    
-    sampleTime_=DEFAULT_SAMPLE_TIME;
-    supplyVoltage_=DEFAULT_VOLTAGE;
-    
+Motor(fwdPin, revPin, DEFAULT_VOLTAGE),
+Encoder(interruptPin, quadraturePin),
+speedController(SPEEDKP, SPEEDKI, SPEEDKD),
+angleController(ANGLEKP, ANGLEKI, ANGLEKD)
+{   
     //Initial angle is established at this moment
-    lastAngle_=getAngle();
-    lastTime_=millis();
-    lastVoltage_=0;
+    //so we can calculate the speed even on the first call of update()
+    speedController.tolerate(1);
+    angleController.tolerate(1);
+    lastAngle=position();
+    lastTime=millis();
+    data.target=0;
 }
 
 boolean NXTServo::update(){
-    long time = millis();
-    if(time-lastTime_<=sampleTime_){
+
+    if(millis()-lastTime<=sampleTime){
         return false;
     }
-    
-    float angle=getAngle();
-    
-    PIDvalues vals;
-    
-    if(controllerMode_==SPEED) {
-        float speed=(angle-lastAngle_)/((float)(time-lastTime_)/1000.0)*60/360;
-        applyVoltage(speedController_(target_, input, time, vals));
+
+    long ms=millis();
+
+    float angle = position();
+
+    data.time = ms/1000.0;
+
+    if(currentState==SPEED) {
+        data.input = ((angle-lastAngle)/360.0) / 
+                        ((data.time-lastTime/1000.0)/60.0);
+
+        Motor::turn(speedController.compute(data));
         
-    } else if(controllerMode_==ANGLE) {
-        
-        applyVoltage(computePID(angle, dTime, kpa_, kia_, kda_));
-        
-    } else if(controllerMode_==OFF) {
-        data_.output=lastVoltage_;
-        data_.input=0;
-        data_.setpoint=0;
-        data_.p=0;
-        data_.i=0;
-        data_.d=0;
+    } else if(currentState==ANGLE) {
+        data.input=angle;
+        Motor::turn(angleController.compute(data));
     }
-    data_.time=(float)time/1000.0;
     
-    lastTime_ = time;
-    lastAngle_ = angle;
+    lastTime = ms;
+    lastAngle = angle;
 
     return true;
 }
 
-float NXTServo::getAngle(){
+float NXTServo::position(){
     long pos=read();
     return (float)pos/2.0;
 }
 
-void NXTServo::sampleTime(int time){
-    sampleTime_=time;
+void NXTServo::setSampleTime(int time){
+    sampleTime=time;
 }
 
 void NXTServo::tuneSpeedPID(float kp, float ki, float kd){
-    speedController_.tune(kp,ki,kd);
+    speedController.tune(kp,ki,kd);
 }
 
 void NXTServo::tuneAnglePID(float kp, float ki, float kd){
-    angleController_.tune(kp,ki,kd);
+    angleController.tune(kp,ki,kd);
 }
 
-void NXTServo::voltage(float voltage){
-    controllerMode_=OFF;
-    applyVoltage(voltage);
-}
 
-void NXTServo::supplyVoltage(float supplyVoltage){
-    supplyVoltage_=supplyVoltage;
-}
-
-void NXTServo::spinAt(float power){
-    controllerMode_=OFF;
-    applyVoltage(power*supplyVoltage_);
-}
-
-void NXTServo::applyVoltage(float voltage){
-    if      (voltage >  supplyVoltage_)
-        voltage =   supplyVoltage_;
-    
-    else if (voltage < -supplyVoltage_)
-        voltage =  -supplyVoltage_;
-    
-    lastVoltage_=voltage;
-    Motor::spinAt(voltage/supplyVoltage_);
-}
-
-void NXTServo::speed(float speed){
-    target_=speed;
-    controllerMode_=SPEED;
+void NXTServo::turnAt(float speed){
+    data.target=speed;
+    currentState=SPEED;
 }
 
 void NXTServo::brake(){
-    speed(0);
+    turnAt(0);
 }
 
-void NXTServo::coast(){
-    spinAt(0);
-}
-
-void NXTServo::angle(float angle){
-    target_=angle;
-    controllerMode_=ANGLE;
+void NXTServo::goTo(float angle){
+    data.target=angle;
+    currentState=ANGLE;
 }
 
 void NXTServo::hold(){
-    angle(getAngle());
+    goTo(position());
 }
 
 void NXTServo::reset(){
-    controllerMode_=OFF;
+    currentState=OFF;
     write(0);
-    lastAngle_=0;
-    lastTime_=millis();
+    lastAngle=0;
+    lastTime=millis();
     update();
 }
 
-int NXTServo::sampleTime(){
-    return sampleTime_;
+void NXTServo::turn(float power){
+    currentState=OFF;
+    Motor::turn(power);
 }
 
-MotorData NXTServo::data(){
-    return data_;
+PIDData* NXTServo::getData(){
+    return &data;
 }
-
--- NXTServo.h
+++ NXTServo.h
@@ -14,82 +14,72 @@
 #include "PID.h"
 #include "Encoder.h"
 
-#define DEFAULT_SAMPLE_TIME 1
+#define DEFAULT_SAMPLE_TIME 0
 #define DEFAULT_VOLTAGE 9
-#define MOTOR_DATA 51
 
-struct MotorData{
-    float time, setpoint, input, output, p,i,d;
-};
+#define SPEEDKP 0
+#define SPEEDKI 0
+#define SPEEDKD 0
 
-class NXTServo : public Motor, public Encoder{
+#define ANGLEKP 0
+#define ANGLEKI 0
+#define ANGLEKD 0
+
+class NXTServo : public Motor, private Encoder{
 public:
     
     NXTServo(int fwdPin, int revPin, int interruptPin, int quadraturePin);
     
-    //returns true if something changes meaning sampleTime is passed
-    //should be run immediately after
-    boolean update();
+    //returns true if something changes meaning sampleTime has passed
+    //should be run very frequently
+    bool update();
     
-    void sampleTime(int time);
-    
-    int sampleTime();
-    
-    void supplyVoltage(float supplyVoltage);
-    
+    //takes integer value in millis
+    void setSampleTime(int newSampleTime);
+            
     void tuneSpeedPID(float kp, float ki, float kd);
     
     void tuneAnglePID(float kp, float ki, float kd);
-    
-    void spinAt(float power);
-    
-    //Applies voltage to motor, returns true if voltage was possible
-    void voltage(float voltage);
-    
+        
     //Gets motor cruising at specified speed
-    void speed(float speed); //in rpm
+    void turnAt(float speed); //in rpm
+
     //stops the motor as fast as possible
     void brake();
     
-    void coast();
-    
     //Turns motor to given position
-    void angle(float angle); //in degrees
+    void goTo(float angle); //in degrees
+
     //holds current position
     void hold();
+
+    //return current position in degrees
+    float position();
+
     //uses current position as new 0 reference point
     void reset();
-    
-    MotorData data();
-    
+
+    //override is necessasry to disable pid
+    void turn(float power);
+
+    //returns struct from PID object
+    PIDData* getData();
+        
 private:
     
-    PID speedController_;
-    PID angleController_;
-    
-    MotorData data_;
-    
-    void applyVoltage(float voltage);
-    
-    enum mode{OFF, SPEED, ANGLE};
+    PID speedController;
+    PID angleController;
 
-    mode controllerMode_;
+    PIDData data;
+
+    enum mode {OFF, SPEED, ANGLE};
+
+    mode currentState = OFF;
     
-    int sampleTime_;
+    int sampleTime = DEFAULT_SAMPLE_TIME;
         
-    float supplyVoltage_;
-    
-    float getAngle();
-    
-    float computePID(float input, float dTime,
-                     float kp,    float ki,     float kd);
-    
-    float target_;
-    
-    long lastTime_;
-    float lastAngle_; //used to compute speed
-
-    float lastVoltage_;
+    long lastTime; //in millis, used to check if updating is needed
+    float lastAngle; //both are used to compute speed
     
 };
 
